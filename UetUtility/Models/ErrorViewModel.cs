fdac6ab3971ff70bcb6cdc5f7a773f60be16a02d
AnhDH <anhdhbn@gmail.com>
2019-01-04 17:52:44 +0700

    Add project files.

---
+++ UetUtility/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UetUtility.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
